76181b4e2ba18309d49481cf17e4963469fee542
RaduMery <tiina.radmer@hotmail.com>
2022-09-30 13:25:53 +0300

    Basic launch mechanic and particle asset back.

--- Assets/Scripts/VFXcontroller.cs
+++ Assets/Scripts/VFXcontroller.cs
@@ -6,6 +6,10 @@ public class VFXcontroller : MonoBehaviour
 {
     public VelocityCounter velocityCounter;
 
+    //SerializeField[GameObject]
+    public GameObject vfx;
+    public Transform elbowR;
+
     private void Start()
     {
         velocityCounter.OnMove += Moved;
@@ -18,7 +22,8 @@ public class VFXcontroller : MonoBehaviour
 
     void Moved(float speed)
     {
-        
+        Instantiate(vfx, elbowR.position + new Vector3(0,1,0),Quaternion.identity);
+        Debug.Log(velocityCounter.velocity.magnitude);
     }
 
 }
