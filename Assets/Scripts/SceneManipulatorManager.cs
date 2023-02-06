a5bfe6bb2b957e04ee8ac2bad2f7777fed7f3fc7
RaduMery <tike.h@hotmail.com>
2023-02-06 13:42:29 +0200

    Update SceneManipulatorManager.cs

--- Assets/Scripts/SceneManipulatorManager.cs
+++ Assets/Scripts/SceneManipulatorManager.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class SceneManipulatorManager : MonoBehaviour
 {
+    [Header("Projector")]
+    public GameObject projector;
+    // placement adjustments for projector
+    // position X, Y, Z
+    // rotation X
     
-    // Start is called before the first frame update
+    public GameObject postProsessing;
+    // what parameters i want to manually manipulate?
+    // can i substitute the "GameObject" with someting more specific?
+
+    [Header("Planes")]
+    public GameObject frontPlane;
+    // color, shader, opacity, mask
+    public GameObject backPlane;
+    //color, sheder, PNGvideoClip,
+
+    [Header("Shaders")]
+    public GameObject something;
+    // list of different shaders
+    // what is the best way to access and manipulate them
+
+    [Header("MoCapClips")]
+    public GameObject videoClips;
+    //List of clips or clip_01, clip_02?
+
     void Start()
     {
         
